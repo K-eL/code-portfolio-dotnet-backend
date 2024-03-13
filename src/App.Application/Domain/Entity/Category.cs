3756b7f776dc7e6f01b4e9ac3f87df5b8efce53b
K-eL <kael81@gmail.com>
2024-03-13 18:03:45 +0100

    fix: fix file formatting

--- src/App.Application/Domain/Entity/Category.cs
+++ src/App.Application/Domain/Entity/Category.cs
@@ -1,14 +1,14 @@
 namespace App.Application.Domain.Entity
 {
-    public class Category
-    {
-        public Category(string name, string description)
-        {
-            Name = name;
-            Description = description;
-        }
+	public class Category
+	{
+		public Category(string name, string description)
+		{
+			Name = name;
+			Description = description;
+		}
 
-        public string Name { get; private set; }
-        public string Description { get; private set; }
-    }
+		public string Name { get; private set; }
+		public string Description { get; private set; }
+	}
 }
